e204bd7b1833726b06e73c253adebd623656c79c
Kris Payne <krispayne@me.com>
2025-07-31 16:41:35 -0500

    add robots.txt and sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,22 @@
+const faviconsPlugin = require("eleventy-plugin-gen-favicons");
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+const EleventyPluginRobotsTxt = require("eleventy-plugin-robotstxt");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPlugin(faviconsPlugin, {});
+    eleventyConfig.addPlugin(sitemap, {
+        sitemap: {
+        hostname: "https://links.krispayne.com",
+        },
+    });
+  
+  /** @type {import("eleventy-plugin-robotstxt/typedefs.js").EleventyPluginRobotsTxtOptions} */
+  const eleventyPluginRobotsTxtOptions = {
+      sitemapURL: "https://links.krispayne.com/sitemap.xml",
+      shouldBlockAIRobots: true,
+    };
+  eleventyConfig.addPlugin(EleventyPluginRobotsTxt,eleventyPluginRobotsTxtOptions);
     
     return {
         dir: {
